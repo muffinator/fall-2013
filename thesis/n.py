20ce4bc1603f9599b03d5e350fbff5635c4fc889
jomg <jomg@mit.edu>
2014-12-25 17:31:41 -0500

    added new flexible methods to sim.py, deleted sim.net, got n.py pretty much all the way working

--- thesis/n.py
+++ thesis/n.py
@@ -2,66 +2,8 @@
 from sim import *
 from random import *
 
-def gen(num):
-    seed()
-    rmatrix=[[None for x in range(num)] for x in range(num)]
-    count=0
-    for r in range(num):
-        for x in range(r,num):
-            tmp = randint(0,1)
-            if tmp==1:
-                rmatrix[x][r]=rmatrix[r][x]=1
-        count+=1
-    return rmatrix
 
-def write(netlist,num):
-    rmatrix=gen(num)
-    netlist=open(netlist, 'w')
-    netlist.write('mynetlist\n')
-    val=str(1)
-    numr=0
-    for x in range(num):
-        if sum(filter(None,rmatrix[x]))<1:
-            return 0
-        for y in range(x,num):
-            if rmatrix[x][y]:
-                rmatrix[x][y]=randint(1,100)
-                rmatrix[y][x]=rmatrix[x][y]
-                val = str(rmatrix[x][y])
-                s = x
-                e = y
-                if s==e:
-                    e = -1
-                netlist.write('R'+str(numr)+' '+str(s+1)+' '+str(e+1)+' '+val+'\n')
-                numr+=1
-    netlist.write(""".control
-    op
-    print """)
-    for elem in range(num):
-        netlist.write('v('+str(elem+1)+') ')
-    netlist.write("""
-    .endc
-    .end
-    """.format(globals()))
-    netlist.close()
-    if numr<num+1:
-        return 0
-    return rmatrix 
-
-def printMatrix(m):
-    for row in m:
-        concat=''
-        for entry in row:
-            if entry==None:
-                print entry,'   ',
-            else:
-                print('{:.2e}'.format(entry)),
-        print ' '
-
-rmatrix=0
-while (rmatrix==0):
-    rmatrix=write('netlist.cir',4)
-    print 'failed'
+matrix=writeRandomNet('netlist.cir',4)
 printMatrix(rmatrix)
 network=runSim('netlist','output')
 nodes=sorted(network.keys())
@@ -104,13 +46,15 @@ for node in range(nuno):
             c2=e/(1.0-e)
         else:
             c2=1E19
-        print 's: '+str(s)+' c1: '+str(c1)+' e: '+str(e)+' c2: '+str(c2)
+        #print 's: '+str(s)+' c1: '+str(c1)+' e: '+str(e)+' c2: '+str(c2)
         if node==subnode or c1==c2==0.0:
             pass
         else:
             i = -1*twod[node][subnode]['vt'+str(node+1)]
-            r[node][subnode]=1/i/((c2+c1*c2)/(c2+c1+1))
-
+            ir= -1*twod[subnode][node]['vt'+str(subnode+1)]
+            r[node][subnode]=(c1+c2+1)/(i*(c2+c1*c2))
+            print node, subnode, r[node][subnode]-(c1+c2+1)/(ir*(c1+c2*c1))
+\
 printMatrix(rmatrix)
 print '\n'
 a = range(nuno)
@@ -134,37 +78,9 @@ for node in a:
         if rng>0:
             r[node][node]=rng
         if rng>100:
-            print rp, irp, req
+            print rp, irp-req
 
 printMatrix(r)
-
-def writeJason(name,network,group):
-    jason=open(name+'.json', 'w')
-    jason.write("""{
-        "nodes":[
-    """)
-    for n in range(len(network)):
-        jason.write('   {"name":"'+str(n)+'","group":'+str(group)+'},\n')
-    jason.write('   {"name":"g","group":'+str(group+1)+'}\n')
-    jason.write("""],
-        "links":[
-    """)
-    chewed=[]
-    for n in range(len(network)):
-        for m in range(n,len(network)):
-            if network[n][m]:
-                if n<>m:
-                    chewed+=[[str(n),str(m),str(int(1./(network[n][m]))+1)]]
-                else:
-                    chewed+=[[str(n),str(len(network)),str(int(1./network[n][n])+1)]]
-    for bite in chewed:
-        jason.write('   {"source":'+bite[0]+',"target":'+bite[1]+',"value":'+bite[2]+'}')
-        if chewed.index(bite)<(len(chewed)-1):
-            jason.write(',\n')
-        else:
-            jason.write('\n')
-    jason.write(']\n}')           
-    jason.close()
 
 writeJason('resistor',r,1)
 writeJason('given',rmatrix,3)
--- thesis/sim.py
+++ thesis/sim.py
@@ -1,4 +1,5 @@
 #CC-BY-SA Josh Gordonson
+from random import *
 import subprocess
 
 def runSim(target,results):
@@ -40,3 +41,94 @@ def insertProbe2(target,nodes,groundNodes):
     netlist.write("".join(contents))
     netlist.close()
     return contents
+
+def printMatrix(m): 
+    for row in m: 
+        concat='' 
+        for entry in row: 
+            if entry==None: 
+                print entry,'   ', 
+            else: 
+                print('{:.2e}'.format(entry)), 
+        print ' ' 
+
+def gen(num):
+    seed()
+    rmatrix=[[None for x in range(num)] for x in range(num)]
+    count=0
+    for r in range(num):
+        for x in range(r,num):
+            tmp = randint(0,1)
+            if tmp==1:
+                rmatrix[x][r]=rmatrix[r][x]=1
+        count+=1
+    return rmatrix
+
+def writeRandomNet(netlist,num):
+    rmatrix=gen(num)
+    netlist=open(netlist, 'w')
+    netlist.write('mynetlist\n')
+    val=str(1)
+    numr=0
+    for x in range(num):
+        if sum(filter(None,rmatrix[x]))<1:
+            return 0
+        for y in range(x,num):
+            if rmatrix[x][y]:
+                rmatrix[x][y]=randint(1,100)
+                rmatrix[y][x]=rmatrix[x][y]
+                val = str(rmatrix[x][y])
+                s = x
+                e = y
+                if s==e:
+                    e = -1
+                netlist.write('R'+str(numr)+' '+str(s+1)+' '+str(e+1)+' '+val+'\n')
+                numr+=1
+    netlist.write(""".control
+    op
+    print """)
+    for elem in range(num):
+        netlist.write('v('+str(elem+1)+') ')
+    netlist.write("""
+    .endc
+    .end
+    """.format(globals()))
+    netlist.close()
+    if numr<num+1:
+        return 0
+    return rmatrix
+
+def randomNet(netlist,num):
+    net=0
+    while (net==0):
+        net=writeRandomNet(netlist,num)
+    return net
+
+def writeJason(name,network,group):
+    jason=open(name+'.json', 'w')
+    jason.write("""{
+        "nodes":[
+    """)
+    for n in range(len(network)):
+        jason.write('   {"name":"'+str(n)+'","group":'+str(group)+'},\n')
+    jason.write('   {"name":"g","group":'+str(group+1)+'}\n')
+    jason.write("""],
+        "links":[
+    """)
+    chewed=[]
+    for n in range(len(network)):
+        for m in range(n,len(network)):
+            if network[n][m]:
+                if n<>m:
+                    chewed+=[[str(n),str(m),str(int(1./(network[n][m]))+1)]]
+                else:
+                    chewed+=[[str(n),str(len(network)),str(int(1./network[n][n])+1)]]
+    for bite in chewed:
+        jason.write('   {"source":'+bite[0]+',"target":'+bite[1]+',"value":'+bite[2]+'}')
+        if chewed.index(bite)<(len(chewed)-1):
+            jason.write(',\n')
+        else:
+            jason.write('\n')
+    jason.write(']\n}')
+    jason.close()
+
