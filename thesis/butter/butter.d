1c38bdc82f3f4ec65d246cf4245f8cc4cc9058b2
jomg <jomg@mit.edu>
2015-03-10 00:39:09 -0400

    added butter stm32 project

---
+++ thesis/butter/butter.d
@@ -0,0 +1,16 @@
+butter.o: butter.c libopencm3/include/libopencm3/stm32/rcc.h \
+ libopencm3/include/libopencm3/cm3/common.h \
+ /opt/ARM/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /opt/ARM/arm-none-eabi/include/stdint.h \
+ /opt/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /opt/ARM/arm-none-eabi/include/sys/features.h \
+ /opt/ARM/lib/gcc/arm-none-eabi/4.8.4/include/stdbool.h \
+ libopencm3/include/libopencm3/stm32/memorymap.h \
+ libopencm3/include/libopencm3/stm32/f3/memorymap.h \
+ libopencm3/include/libopencm3/cm3/memorymap.h \
+ libopencm3/include/libopencm3/stm32/f3/rcc.h \
+ libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ libopencm3/include/libopencm3/stm32/gpio.h \
+ libopencm3/include/libopencm3/stm32/f3/gpio.h \
+ libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
