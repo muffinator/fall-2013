8e6329661e61b89e0cb81fa5189f651441ce7fcf
jomg <jomg@mit.edu>
2015-07-19 10:27:30 -0400

    peak detecting and thiiings

--- thesis/tsp/sugar.py
+++ thesis/tsp/sugar.py
@@ -4,17 +4,19 @@ from pylab import *
 ser = serial.Serial("/dev/ttyACM0")
 
 def medicine(chan):
-    ser.write(['g',(0<<chan)])
+    #ser.write(['g',(0<<chan)])
     ser.write(['m',chan])
-    ser.write(['s',10])
-    a = ser.readline()
+    ser.write(['s',100])
+    a = ser.read(1920)
     b = [((ord(a[2*x+1])<<8)+ord(a[2*x])) for x in range(len(a)/2)]
    # print(x,a[0:10])
-    print(x,a[21:23],((ord(a[21])<<8)+ord(a[22]))*5.04/1024)
+    print(chan,max(b),min(b),max(b)-min(b))
     plot(range(len(b)+70),b[2:-2]+74*[b[-2]],label=chan)
 
-for x in range(8):
-    medicine(x)
-legend(loc="lower right")
+#for x in range(8):
+ #   medicine(x)
+#medicine(5)
+medicine(2)
+#legend(loc="lower right")
 show()
 ser.close()
--- thesis/tsp/teaspoon.c
+++ thesis/tsp/teaspoon.c
@@ -33,6 +33,7 @@
 #include <libopencm3/stm32/dac.h>
 #include <libopencm3/stm32/usart.h>
 #include "usb.c"
+#include "sin.c"
 
 #define mydma DMA2
 #define mystream DMA_STREAM1
@@ -49,11 +50,11 @@
 
 static volatile char message = 'a';
 static volatile uint8_t ready = 0;
-static volatile uint16_t datas[8000];
-static volatile uint16_t datuh[8000];
-static volatile uint16_t rearranged[500];
+static volatile uint16_t datas[16000];
+static volatile uint16_t rearranged[1000];
 static volatile uint8_t send=0;
 static volatile uint8_t muxpin[8]={5,7,6,4,2,1,0,3};
+volatile uint16_t idx=0;
 static usbd_device *usb_device;
 
 static void gpio_setup(void)
@@ -66,8 +67,8 @@ static void gpio_setup(void)
     */
     //------------	PC13: !CS
     rcc_periph_clock_enable(RCC_GPIOC);
-    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
-    gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO13);
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1|GPIO13);
+    gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO1|GPIO13);
 
     //------------	PB6-PB8,PB9: S0-S2,SE
 	rcc_periph_clock_enable(RCC_GPIOB);
@@ -87,6 +88,8 @@ static void gpio_setup(void)
     //------------	A8: Clock
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_OTGFS);
+
+    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO4);
 
     gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO11 | GPIO12);
     gpio_set_af(GPIOA, GPIO_AF10, GPIO11 | GPIO12);
@@ -117,7 +120,7 @@ static void dma_setup(void)
     /* The array v[] is filled with the waveform data to be output */
     //	dma_set_memory_address(mydma, mystream, (uint32_t) &GPIOB_ODR);
     dma_set_memory_address(mydma, mystream, (uint32_t) datas);
-    dma_set_number_of_data(mydma, mystream, 8000);
+    dma_set_number_of_data(mydma, mystream, 16000);
     //dma_disable_transfer_complete_interrupt(mydma, mystream);
     dma_channel_select(mydma, mystream, mychannel);
     dma_enable_direct_mode(mydma, mystream);
@@ -125,18 +128,40 @@ static void dma_setup(void)
     //dma_enable_direct_mode_error_interrupt(mydma, mystream);
     dma_enable_stream(mydma, mystream);
 
-    /*
+
+
     rcc_periph_clock_enable(RCC_DMA1);
-    nvic_enable_irq(NVIC_DMA1_STREAM1_IRQ);
-    //disable dma
-    DMA1_S1CR = 0;
-    DMA1_S1CR = DMA_SxCR_PL_HIGH | DMA_SxCR_MSIZE_16BIT | DMA_SxCR_PSIZE_16BIT | DMA_SxCR_MINC;
-    DMA1_S1PAR = (uint32_t *) 0x40020410;//GPIOB_IDR;
-    DMA1_S1M0AR = (uint32_t *)datas;
-    DMA1_S1NDTR = 8;
-    DMA1_S1CR |= DMA_SxCR_CHSEL_3|DMA_SxCR_CIRC|DMA_SxCR_TCIE;
-    DMA1_S1CR |= 1;
-    */
+	nvic_enable_irq(NVIC_DMA1_STREAM5_IRQ);
+	dma_stream_reset(DMA1, DMA_STREAM5);
+	dma_set_priority(DMA1, DMA_STREAM5, DMA_SxCR_PL_HIGH);
+	dma_set_memory_size(DMA1, DMA_STREAM5, DMA_SxCR_MSIZE_8BIT);
+	dma_set_peripheral_size(DMA1, DMA_STREAM5, DMA_SxCR_PSIZE_8BIT);
+	dma_enable_memory_increment_mode(DMA1, DMA_STREAM5);
+	dma_enable_circular_mode(DMA1, DMA_STREAM5);
+	dma_set_transfer_mode(DMA1, DMA_STREAM5,
+				DMA_SxCR_DIR_MEM_TO_PERIPHERAL);
+	/* The register to target is the DAC1 8-bit right justified data
+	   register */
+	dma_set_peripheral_address(DMA1, DMA_STREAM5, (uint32_t) &DAC_DHR8R1);
+	/* The array v[] is filled with the waveform data to be output */
+	dma_set_memory_address(DMA1, DMA_STREAM5, (uint32_t) stim);
+	dma_set_number_of_data(DMA1, DMA_STREAM5, 800);
+	dma_enable_transfer_complete_interrupt(DMA1, DMA_STREAM5);
+	dma_channel_select(DMA1, DMA_STREAM5, DMA_SxCR_CHSEL_7);
+	dma_enable_stream(DMA1, DMA_STREAM5);
+}
+
+static void dac_setup(void)
+{
+	/* Enable the DAC clock on APB1 */
+	rcc_periph_clock_enable(RCC_DAC);
+	/* Setup the DAC channel 1, with timer 2 as trigger source.
+	 * Assume the DAC has woken up by the time the first transfer occurs */
+	dac_buffer_enable(CHANNEL_1);
+	dac_trigger_enable(CHANNEL_1);
+	dac_set_trigger_source(DAC_CR_TSEL1_T5);
+	dac_dma_enable(CHANNEL_1);
+	dac_enable(CHANNEL_1);
 }
 
 /* Timer Setup */
@@ -200,46 +225,48 @@ static void timer2_setup(void)
     timer_enable_counter(TIM2);
 }
 
-static void timer3_setup(void)
+static void timer5_setup(void)
 {
-	rcc_periph_clock_enable(RCC_TIM3);
-	nvic_enable_irq(NVIC_TIM3_IRQ);
-	timer_reset(TIM3);
-	timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT,TIM_CR1_CMS_EDGE,TIM_CR1_DIR_UP);
-	timer_set_prescaler(TIM3, 0);
-	timer_disable_preload(TIM3);
-	timer_continuous_mode(TIM3);
-	timer_set_period(TIM3, 165);
-    //timer_disable_oc_output(TIM3, TIM_OC2);
-    // timer_update_on_overflow(TIM3);
-	//timer_set_dma_on_update_event(TIM3);
-	timer_set_oc_mode(TIM3, TIM_OC1, TIM_OCM_PWM2);
-	timer_disable_oc_output(TIM3, TIM_OC1);
-
-	timer_set_oc_fast_mode(TIM3, TIM_OC1);
-	timer_set_oc_value(TIM3, TIM_OC1, 145);
-
-	//timer_disable_oc_clear(TIM3, TIM_OC1);
-	//timer_disable_oc_preload(TIM3, TIM_OC1);
-    // timer_set_oc_fast_mode(TIM3, TIM_OC2);
-    // timer_set_oc_value(TIM3, TIM_OC2, 10);
-	//timer_disable_preload(TIM3);
-	timer_enable_counter(TIM3);
+	rcc_periph_clock_enable(RCC_TIM5);
+	nvic_enable_irq(NVIC_TIM5_IRQ);
+	timer_reset(TIM5);
+	timer_set_mode(TIM5, TIM_CR1_CKD_CK_INT,TIM_CR1_CMS_EDGE,TIM_CR1_DIR_UP);
+	timer_set_prescaler(TIM5, 0);
+	timer_disable_preload(TIM5);
+	timer_continuous_mode(TIM5);
+	timer_set_period(TIM5, 165);
+    //timer_disable_oc_output(TIM5, TIM_OC2);
+    // timer_update_on_overflow(TIM5);
+	//timer_set_dma_on_update_event(TIM5);
+	timer_set_oc_mode(TIM5, TIM_OC1, TIM_OCM_PWM2);
+	timer_disable_oc_output(TIM5, TIM_OC2 | TIM_OC3 | TIM_OC4);
+    timer_enable_oc_output(TIM5, TIM_OC1);
+    timer_disable_oc_clear(TIM5, TIM_OC1);
+    timer_disable_oc_preload(TIM5, TIM_OC1);
+	timer_set_oc_fast_mode(TIM5, TIM_OC1);
+	timer_set_oc_value(TIM5, TIM_OC1, 145);
+    timer_set_master_mode(TIM5, TIM_CR2_MMS_COMPARE_OC1REF);
+	//timer_disable_oc_clear(TIM5, TIM_OC1);
+	//timer_disable_oc_preload(TIM5, TIM_OC1);
+    // timer_set_oc_fast_mode(TIM5, TIM_OC2);
+    // timer_set_oc_value(TIM5, TIM_OC2, 10);
+	//timer_disable_preload(TIM5);
+	timer_enable_counter(TIM5);
 }
 
 /* Timer 3 ISR */
 
-void tim3_isr(void)
+void tim5_isr(void)
 {
-    if (timer_get_flag(TIM3, TIM_SR_UIF)){
-        timer_clear_flag(TIM3, TIM_SR_UIF);
+    if (timer_get_flag(TIM5, TIM_SR_UIF)){
+        timer_clear_flag(TIM5, TIM_SR_UIF);
         TIM2_EGR |= TIM_EGR_UG;
         gpio_clear(cs);
         TIM1_EGR |= TIM_EGR_UG;
     }
 
-    if (timer_get_flag(TIM3, TIM_SR_CC1IF)) {
-        timer_clear_flag(TIM3, TIM_SR_CC1IF);
+    if (timer_get_flag(TIM5, TIM_SR_CC1IF)) {
+        timer_clear_flag(TIM5, TIM_SR_CC1IF);
         gpio_set(cs);
     }
 }
@@ -253,12 +280,12 @@ void dma2_stream1_isr(void)
         dma_clear_interrupt_flags(mydma, mystream, DMA_TCIF);
         /* Toggle PC1 just to keep aware of activity and frequency. */
         //gpio_toggle(GPIOD, GPIO12);
-        timer_disable_counter(TIM3);
+        timer_disable_counter(TIM5);
         //maximum packet size of 64 bytes
         int n;
         int in;
         char pin=(0<<0);
-        for(n=0;n<500;n++)
+        for(n=0;n<1000;n++)
         {
             rearranged[n]=0;
             for(in=0;in<16;in++)
@@ -268,7 +295,7 @@ void dma2_stream1_isr(void)
             }
             //rearranged[n]=rearranged[n]>>1;
         }
-        for(n=0;n<30;n++)		//we want 30 packets
+        for(n=0;n<60;n++)		//we want 30 packets
         {
             while (usbd_ep_write_packet(usb_device, 0x82, (const void *)&rearranged[n*16],32)==0);
             //While (usbd_ep_write_packet(usb_device, 0x82, (const void *)&(datas[n*32]), 64)==0);
@@ -283,7 +310,8 @@ void dma2_stream1_isr(void)
         test[4]='/';
         test[5]='n';
         while (usbd_ep_write_packet(usb_device, 0x82, (const void *)&test[2], 2)==0);
-        TIM3_EGR |= TIM_EGR_UG;
+        dac_disable(CHANNEL_1);
+        TIM5_EGR |= TIM_EGR_UG;
     }
     if (dma_get_interrupt_flag(mydma, mystream, DMA_DMEIF)) {
         dma_clear_interrupt_flags(mydma, mystream, DMA_DMEIF);
@@ -291,6 +319,16 @@ void dma2_stream1_isr(void)
         gpio_set(GPIOD, blue);
     }
 }
+
+void dma1_stream5_isr(void)
+{
+    if (dma_get_interrupt_flag(DMA1, DMA_STREAM5, DMA_TCIF)) {
+        dma_clear_interrupt_flags(DMA1, DMA_STREAM5, DMA_TCIF);
+        /* Toggle PC1 just to keep aware of activity and frequency. */
+        gpio_toggle(GPIOD, green);
+    }
+}
+
 
 static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
 {
@@ -302,8 +340,9 @@ static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
         //while (usbd_ep_write_packet(usbd_dev, 0x82, buf, len)==0);
         if(buf[0]=='s')
         {
-            timer_set_period(TIM3, 165+buf[1]);
-            timer_enable_counter(TIM3);
+            dac_enable(CHANNEL_1);
+            timer_set_period(TIM5, 165+buf[1]);
+            timer_enable_counter(TIM5);
             timer_enable_counter(TIM2);
             timer_enable_counter(TIM1);
         }
@@ -351,13 +390,15 @@ int main(void)
 
     timer2_setup();
     timer1_setup();
-    timer3_setup();
-    timer_enable_irq(TIM3, TIM_DIER_UIE|TIM_DIER_CC1IE);
-    timer_disable_counter(TIM3);
+    timer5_setup();
+    timer_enable_irq(TIM5, TIM_DIER_UIE|TIM_DIER_CC1IE);
+    timer_disable_counter(TIM5);
     timer_disable_counter(TIM2);
     timer_disable_counter(TIM1);
     dma_setup();
-
+    dac_setup();
+    dac_disable(CHANNEL_1);
+    
     usb_device = usbd_init(&otgfs_usb_driver, &dev, &config,usb_strings, 3,
         usbd_control_buffer, sizeof(usbd_control_buffer));
     usbd_register_set_config_callback(usb_device, cdcacm_set_config);
