ca5571e219fbb2db094212d36e32dc0456b5b3d9
jomg <jomg@mit.edu>
2015-08-21 23:16:07 -0400

    started working on oz

---
+++ thesis/oz/sim.py
@@ -0,0 +1,202 @@
+#CC-BY-SA Josh Gordonson
+from random import *
+import subprocess
+import cmath
+import math
+
+def runSim(target,results,source='DC'):
+    output=open(results+'.txt', 'w')
+    a=subprocess.call(['ngspice','-b',target+'.cir'], stdout=output) #run ngspice
+    output.close()
+    output=open(results+'.txt', 'r')
+    if source=='DC':
+        a=output.read().splitlines()[7:] #get rid of the non-data output
+        output.close()
+        vars={}
+        for node in a:
+            entry=node.replace('(','').replace(')','') #formatting
+            exec(entry) #assign the resulting text values to the text variables
+            nodeval = entry.split('=') #make it easy to make a dictionary of the var/vals
+            vars.update({nodeval[0].strip()[1:]:float(nodeval[1].strip())})
+    else:
+        a=output.read().splitlines()
+        output.close()
+        takedata=0
+        datas=[]
+        vars={}
+        for line in a:
+            if ("-----" in line) or line=="\x0c":
+                continue
+            if "mynetlist" in line:
+                takedata=0
+            if "Index" in line:
+                takedata=1
+                if datas!=[]:
+                    vars.update({var:datas})
+                var=line[24:].strip().replace('(','').replace(')','')[1:]
+                datas=[]
+                flist=[]
+                continue
+            if takedata:
+                line=line.replace(',','').split('\t')
+                datas+=[complex(float(line[2]),float(line[3]))]
+                flist+=[float(line[1])]
+        vars.update({var:datas})
+        vars.update({'f':flist})
+    return vars
+
+def insertProbe(target,node,source='DC'):
+    netlist=open(target+'.cir', 'r')
+    contents=netlist.readlines()
+    netlist.close()
+    contents.insert(1,'V '+node+' 0 '+source+' 1\n')
+    contents.insert(-3,'print i(V)\n')
+    netlist=open(target+'-t.cir', 'w')
+    netlist.write("".join(contents))
+    netlist.close()
+    return contents
+
+# inserts a voltage probe at [nodes] and grounds [groundNodes] in netlist 'target'
+def insertProbe2(target,nodes,groundNodes,probes,source='DC'):
+    netlist=open(target+'.cir', 'r')
+    contents=netlist.readlines()
+    netlist.close()
+    sources=''.join(['V '+x+' 0 '+source+' 1\n' for x in nodes])
+    grounds=''.join(['Vg'+x+' '+x+' 0 DC 0\n' for x in groundNodes])
+    currents=''.join(['    print i(V) ' for x in nodes])
+    voltages=''.join(['v('+x+') ' for x in probes])
+    control=''
+    if source=='AC':
+        control='    AC dec 1 1 100000 \n'
+    contents.insert(1,sources+grounds)
+    contents.insert(-4,control+currents+voltages+'\n')
+    netlist=open(target+'-t.cir', 'w')
+    netlist.write("".join(contents))
+    netlist.close()
+    return contents
+
+def printMatrix(m):
+    if type(m[0][0])==list:
+        for row in m:
+            for freq in range(len(row[0])):
+                for entry in row:
+                    if entry[freq] == None:
+                        print None,'\t\t',
+                    else:
+                        print('{:.2e}'.format(entry[freq]))+'\t',
+                print''
+            print ''
+    else:
+        for row in m:
+            for entry in row:
+                concat=''
+                if entry==None:
+                    print entry,'   ',
+                else:
+                    print('{:.2e}'.format(entry)),
+            print ' '
+	pass
+
+
+def genMatrix(num):
+    seed()
+    rmatrix=[[None for x in range(num)] for x in range(num)]
+    count=0
+    for r in range(num):
+        for x in range(r,num):
+            if x==r:
+                continue
+            tmp = randint(0,1)
+            if tmp==1:
+                rmatrix[x][r]=rmatrix[r][x]=1
+        count+=1
+    for x in range(num):
+        if sum(filter(None,rmatrix[x]))<1:
+            return genMatrix(num)
+    return rmatrix
+
+def writeRandomNet(netlist,num,elements):
+    elemlist=[]
+    wname=[]
+    if 'R' in elements:
+        elemlist+=[genMatrix(num)]
+        wname+=['R']
+    if 'L' in elements:
+        wname+=['L']
+        elemlist+=[genMatrix(num)]
+    if 'C' in elements:
+        wname+=['C']
+        elemlist+=[genMatrix(num)]
+    netlist=open(netlist+'.cir', 'w')
+    netlist.write('mynetlist\n')
+    val=str(1)
+    idx=0
+    if 'C' or 'L' in wname:
+        idx = 0
+    for elem in elemlist:
+        numr=0
+        for x in range(num):
+            for y in range(x,num):
+                if elem[x][y]:
+                    elem[x][y]=int(10**triangular(0,3,1.5))
+                    elem[y][x]=elem[x][y]
+                    val = str(elem[x][y])
+                    s = x
+                    e = y
+                    if s==e:
+                       continue #e = -1
+                    if wname[elemlist.index(elem)]=='C':
+                        netlist.write('C'+str(numr)+' '+str(s+1)+' '+str(e+1)+' '+val+'e-7'+'\n')
+                        netlist.write('Rc'+str(numr)+' '+str(s+1)+' '+str(e+1)+' '+'1e8'+'\n')
+                        elem[x][y]=elem[y][x]=elem[x][y]*1e-7
+                    elif wname[elemlist.index(elem)]=='L':
+                        netlist.write('L'+str(numr)+' '+str(s+1)+' '+'tl'+str(numr)+' '+val+'e-4'+'\n')
+                        netlist.write('Rl'+str(numr)+' '+'tl'+str(numr)+' '+str(e+1)+' '+'1e-5'+'\n')
+                        elem[x][y]=elem[y][x]=elem[x][y]*1e-4
+                    else:
+                        netlist.write('R'+str(numr)+' '+str(s+1)+' '+str(e+1)+' '+val+'\n')
+                    numr+=1
+        idx=1
+    netlist.write(""".control
+    op
+    """)
+    #for elem in range(num):
+        #netlist.write('v('+str(elem+1)+') ')
+    netlist.write("""
+    .endc
+    .end
+    """.format(globals()))
+    netlist.close()
+    if numr<num+1:
+        pass
+        #return 0
+    return elemlist
+
+def writeJason(name,network,group,elem):
+    jason=open(name+'.json', 'w')
+    jason.write("""{
+        "nodes":[
+    """)
+    for n in range(len(network)):
+        jason.write('   {"name":"'+str(n)+'","group":'+str(group)+'}')
+        if n<(len(network)-1):
+            jason.write(',\n')
+    jason.write("""],
+        "links":[
+    """)
+    chewed=[]
+    for n in range(len(network)):
+        for m in range(n,len(network)):
+            if network[n][m]:
+                if n<>m:
+                    chewed+=[[str(n),str(m),str(round(network[n][m],2))]]
+                else:
+                    chewed+=[[str(n),str(len(network)),str(round(network[n][n],2))]]
+    for b in chewed:
+        jason.write('   {"source":'+b[0]+',"target":'+b[1]+',"value":'+b[2]+',"elem":'+str(elem)+'}')
+        if chewed.index(b)<(len(chewed)-1):
+            jason.write(',\n')
+        else:
+            jason.write('\n')
+    jason.write(']\n}')
+    jason.close()
---
+++ thesis/oz/sugar.py
@@ -0,0 +1,121 @@
+import serial
+from random import *
+from sim import *
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+
+ser = serial.Serial("/dev/ttyACM0")
+
+def medicine(drive,chan,freq=1000,adc=0,uglist=[]):
+    nog=0
+    for x in uglist:
+        nog+=(1<<x)
+    ser.write(['g',0xff&(~((1<<chan)|(1<<drive)|nog))])
+    ser.write(['w',drive+1])
+    freq = int(freq)
+    f=[freq&0xff,(freq&0xff00)>>8,(freq&0xff0000)>>16]
+    ser.write(['m',chan])
+    ser.flushInput()
+    ser.write((['s']+f+[adc]))
+    # adc value of 0 is for voltage and 1 for current
+    a = ser.read(2000)
+    b = [5*((ord(a[2*x+1])<<8)+ord(a[2*x]))/1024. for x in range(len(a)/2)]
+    b = b[2:]
+    ft=fft(b)
+    aind=argmax(abs(ft[1:]))+1
+    amp = abs(ft[aind])/500
+    phase = angle(ft[aind])
+    # plot(b)
+    return abs(ft[aind])/500
+
+""" making sure medicine works.... """
+
+n=4
+zp=[]
+w=logspace(2,5,10)
+for x in range(n):
+    zp+=[[100*medicine(x,x,f,0)/medicine(x,x,f,1) for f in w]]
+    # zp+=[[90*medicine(x,x,f,1) for f in w]]
+    # show()
+    # loglog(w,zp[x])
+# show()
+
+zm = [[None for q in range(n)] for e in range(n)]
+printMatrix(zm)
+for x in range(n):
+    o = range(n)
+    o.remove(x)
+    zm[x][x]=zp[x]
+    for y in o:
+        zm[x][y]=[zp[x][f]/medicine(y,x,w[f],0)*medicine(y,y,w[f],0,[x]) for f in range(len(w))]
+        try:
+            if(all(i <= 10000 for i in zm[x][y])):
+                loglog(w,zm[x][y],label=str(x)+','+str(y))
+        except:
+            continue
+    try:
+        if(all(i <= 10000 for i in zm[x][x])):
+            loglog(w,zm[x][x],label=str(x)+','+str(x))
+    except:
+        continue
+printMatrix(zm)
+legend()
+show()
+
+# w=concatenate((logspace(2,3.9,10),logspace(4,5,10)))
+# v=[]
+# i=[]
+# vn=[]
+# rs=[]
+# ls=[]
+# cs=[]
+# z=[]
+# for x in w:
+#     compv = medicine(0,0,x,0)
+#     compi = medicine(0,0,x,1)
+#     #compvn = medicine(n,p,x,0)
+#     v+=[abs(compv)]
+#     i+=[abs(compi)]
+#     #vn+=[abs(compvn)/500]
+#     z+=[v[-1]/i[-1]*100]
+# for f in range(len(z)-1):
+#     slope = (log(z[f+1])-log(z[f]))/(log(w[f+1])-log(w[f]))
+#     if slope < -.8:
+#         if slope > -1.2:
+#             cs+=[1/(z[f]*2*3.1415*w[f])]
+#             print 1/(z[f]*2*3.1415*w[f]),"F", w[f]
+#     if slope < .2:
+#         if slope > -.2:
+#             rs+=[z[f]]
+#             print z[f], "ohm", w[f]
+#     if slope > .8:
+#         if slope < 1.2:
+#             ls+=[z[f]/(2*3.1415*w[f])]
+#             print z[f]*2*3.1415*w[f],"H", w[f]
+#
+#
+# fig, ax1 = plt.subplots()
+#
+# ax2 = ax1.twinx()
+# ax1.semilogx(w, v, 'y-', w,i, 'g-')
+#
+# if(cs):
+#     c = [1/(mean(cs)*2*3.1415*s) for s in w]
+#     ax2.loglog(w,c,'k-')
+# if(rs):
+#     r = [mean(rs) for s in w]
+#     ax2.loglog(w,r,'k-')
+# if(ls):
+#     l = [mean(ls)*2*3.1415*s for s in w]
+#     ax2.loglog(w,l,'k-')
+#
+# ax2.loglog(w, z, 'b-o')
+#
+# ax1.set_xlabel('Frequench (Hz) data')
+# ax1.set_ylabel('Voltage (yellow) Current (Green)', color='g')
+# ax2.set_ylabel('Impedance (Ohms)', color='b')
+# plt.savefig("/home/muffin/Documents/fall-2013/thesis/tsp/plots/fig.png")
+
+ser.close()
